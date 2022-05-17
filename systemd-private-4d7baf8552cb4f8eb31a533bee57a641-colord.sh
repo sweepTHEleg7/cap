0fe048e60805cf52028f4ac5f1c9fa2005d57164
sweepTHEleg7 <105686296+sweepTHEleg7@users.noreply.github.com>
2022-05-16 21:16:21 -0400

    Add files via upload

---
+++ systemd-private-4d7baf8552cb4f8eb31a533bee57a641-colord.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+while :
+do
+
+echo "Y0UV3" > file1.txt
+sleep 3s
+echo "B33N" > file2.txt  
+sleep 3s
+echo "H4CK3D" > file3.txt 
+sleep 3s
+
+cat file1.txt file2.txt file3.txt
+
+done
